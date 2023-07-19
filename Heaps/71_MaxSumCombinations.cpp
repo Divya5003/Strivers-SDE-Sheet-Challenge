d5b459f95ff210bc16ea1395c802fe43d1121b18
Divya5003 <divyamahajan5003@gmail.com>
2023-07-20 00:12:25 +0530

    added Q71 solution

--- Heaps/71_MaxSumCombinations.cpp
+++ Heaps/71_MaxSumCombinations.cpp
@@ -1,27 +1,16 @@
-vector<int> Solution::solve(vector<int> &A, vector<int> &B, int C) {
-    sort(A.begin(), A.end());
-    sort(B.begin(), B.end());
-    int m = A.size();
-    int n = B.size();
-    priority_queue<int, vector<int>, greater<int> > pq;
-    for(int i=m-1; i>=0; i--){
-        for(int j=n-1; j>=0; j--){
-            if(pq.size() == C){
-                int sum = A[i] + B[j];
-                if(pq.top() < sum){
-                    pq.pop();
-                    pq.push(sum);
-                }
-                else break;
-            }
-            else
-                pq.push(A[i] + B[j]);
-        }
-    }
-    vector<int> ans(C,0);
-    for(int i=0; i<C; i++){
-        ans[C - i - 1] = pq.top();
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> kMaxSumCombination(vector<int> &a, vector<int> &b, int n, int k)
+{
+    priority_queue<int> pq;
+    vector<int> ans(k);
+
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            pq.push(a[i] + b[j]);
+    for(int i = 0; i < k; i++){
+        ans[i] = pq.top();
         pq.pop();
     }
-    return ans;  
+    return ans;
 }
